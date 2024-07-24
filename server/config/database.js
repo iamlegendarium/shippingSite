ee5cdf59126f87aa4d2a36d58633a8a6b5f42cad
Iamlegendarium <iamlegendarium@gmail.com>
2024-07-24 13:19:10 +0100

    port

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
-  { host: process.env.HOST, dialect: "mysql", dialectModule: require("mysql2") }
+  { host: process.env.HOST, dialect: "mysql", dialectModule: require("mysql2"), port: 3306 }
 );
 
 sequelize.authenticate()

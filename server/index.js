79194e221a143e66188420e4215e7bb4b0cae7be
iamlegendarium <70863279+iamlegendarium@users.noreply.github.com>
2025-10-18 10:22:34 -0700

    Add Vercel frontend URL to CORS whitelist

--- server/index.js
+++ server/index.js
@@ -30,7 +30,8 @@ app.use(
       "http://127.0.0.1:5500",
       "http://localhost:5500",
       "http://localhost:3000",
-      'https://shipping.onrender.com'
+      'https://shipping.onrender.com',
+      "https://shipping-site-frontend.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
